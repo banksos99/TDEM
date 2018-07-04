31465109844a5d8da2d519b211f2eebdf41d423c
WilaiwanDansakchai <babeii59@gmail.com>
2018-07-04 10:05:51 +0700

    Edit ignore file

--- android/app/src/main/java/com/toyotaasia/tdemconnect/MainApplication.java
+++ android/app/src/main/java/com/toyotaasia/tdemconnect/MainApplication.java
@@ -3,6 +3,7 @@ package com.toyotaasia.tdemconnect;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.rnfs.RNFSPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.calendarevents.CalendarEventsPackage;
@@ -46,6 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNFSPackage(),
             new RNFetchBlobPackage(),
             new CalendarEventsPackage(),
